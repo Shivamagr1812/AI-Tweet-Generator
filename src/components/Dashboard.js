421b656911df5f12a03a0ad6397a818d7e0edaca
Shivam <eshivam360@gmail.com>
2023-07-01 14:52:24 +0530

    changed title

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -48,13 +48,13 @@ export default function Dashboard() {
         <form onSubmit={(e) => handleSubmit(e)}>
             <div className="flex flex-col">
               <label className="sr-only" htmlFor="tweetTitle">
-                Tweet Title
+                Tweet Topic
               </label>
               <input
                 type="text"
                 className="block w-full rounded-md bg-white border border-gray-400 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm px-4 py-2 placeholder-gray-500 my-2 text-gray-900"
                 name="tweetTitle"
-                placeholder="Tweet Title"
+                placeholder="Tweet Topic"
                 id="tweetTitle"
                 value={tweetTitle}
                 onChange={(e) => setTweetTitle(e.target.value)}
