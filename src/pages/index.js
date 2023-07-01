911265cc0acfcb95e207926c4121fdfd1484dde4
Shivam <eshivam360@gmail.com>
2023-07-01 15:08:19 +0530

    modified metatags

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,11 @@ export default function Home() {
     <>
       <Head>
         <title>AI Tweet Generator</title>
-        <meta name="description" content="This is an AI powered Tweet Generator made by Shivam" />
+        <meta name="description" content="This is an AI powered Tweet Generator made by Shivam." />
+        <meta property="og:title" content="AI Tweet Generator"/>
+        <meta property="og:image" content="/twitter-logo.png"/>
+        <meta property="og:description" content="This is an AI powered Tweet Generator made by Shivam."/>
+        <meta property="og:url" content="ai-tweet-generator-shivamagr1812.vercel.app"/>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
